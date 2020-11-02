7e1b50d39cff33f51e7754e47fedfc492a5cbec5
Moe98 <moeadele98@gmail.com>
2020-11-02 23:15:47 +0200

    Add .equals
    I added .equals in the needed classes and that seems to have fixed the issue we had with the hashset which caused the algorithm to run forever.

--- src/core/State.java
+++ src/core/State.java
@@ -7,7 +7,7 @@ public class State {
 	private Location location;
 	private int truckLoad;
 	private SoldiersMap soldiersMap;
-	
+
 	public State(Location location, int truckLoad, SoldiersMap soldiersMap) {
 		super();
 		this.location = location;
@@ -18,27 +18,27 @@ public class State {
 	public Location getLocation() {
 		return location;
 	}
-	
+
 	public void setLocation(Location location) {
 		this.location = location;
 	}
-	
+
 	public int getTruckLoad() {
 		return truckLoad;
 	}
-	
+
 	public void setTruckLoad(int truckLoad) {
 		this.truckLoad = truckLoad;
 	}
-	
+
 	public SoldiersMap getSoldiers() {
 		return soldiersMap;
 	}
-	
+
 	public void setSoldiers(SoldiersMap soldiersMap) {
 		this.soldiersMap = soldiersMap;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		StringBuilder code = new StringBuilder();
@@ -49,8 +49,14 @@ public class State {
 	}
 
 	@Override
+	public boolean equals(Object obj) {
+		return location.equals(((State) obj).getLocation()) && truckLoad == ((State) obj).getTruckLoad()
+				&& soldiersMap.equals(((State) obj).getSoldiers());
+	}
+
+	@Override
 	public String toString() {
 		return "State [location=" + location + ", truckLoad=" + truckLoad + ", soldiersMap=" + soldiersMap + "]";
 	}
-	
+
 }
