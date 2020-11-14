18627204dc1e3b8618baeb964fbc285f1b0c79a6
YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
2020-11-15 00:36:28 +0200

    added abstract generalsearch
    added abstraction level and fixing hirec.

--- src/core/Problem.java
+++ src/core/Problem.java
@@ -1,195 +1,28 @@
 package core;
 
-import java.util.Arrays;
-
-import data.Location;
-import data.Soldier;
-import data.SoldiersMap;
-
-public class Problem {
-	private int n, m, c;
-	private State initialState;
-	private Location ethanLocation, submarineLocation;
-	private Soldier[] soldiers;
+public abstract class Problem {
 	private String[] operators;
+	private State initialState;
 
-	public Problem(int n, int m, int c, Location ethanLocation, Location submarineLocation, Soldier[] soldiers,
-			String[] operators) {
+	public Problem(String[] operators, State initialState) {
 		super();
-		this.n = n;
-		this.m = m;
-		this.c = c;
-		this.initialState = new State(ethanLocation, 0, new SoldiersMap(soldiers.length));
 		this.operators = operators;
-		this.ethanLocation = ethanLocation;
-		this.submarineLocation = submarineLocation;
-		this.soldiers = soldiers;
+		this.initialState = initialState;
 	}
-
-	public boolean goalTest(State state) {
-		return state.getSoldiers().isAllRescued() && state.getTruckLoad() == 0
-				&& state.getLocation().equals(submarineLocation);
-	}
-
-	public State stateAfterDrop(State state) {
-		Location location = state.getLocation();
-		int truckLoad = state.getTruckLoad();
-		SoldiersMap soldiersMap = state.getSoldiers();
-
-		SoldiersMap newSoldiers = new SoldiersMap(soldiersMap.getNumOfSoldiers(), soldiersMap.getBitmap());
-		if (location.equals(submarineLocation) && truckLoad > 0)
-			truckLoad = 0;
-		return new State(Location.getNewLocation(location, "DROP"), truckLoad, newSoldiers);
-	}
-
-	public State stateAfterPick(State state) {
-		Location location = state.getLocation();
-		int truckLoad = state.getTruckLoad();
-		SoldiersMap soldiersMap = state.getSoldiers();
-
-		int newTruckLoad = truckLoad;
-		SoldiersMap newSoldiersMap = new SoldiersMap(soldiersMap.getNumOfSoldiers(), soldiersMap.getBitmap());
-
-		int soldierIdx = getSoldierIndexAtLocation(location);
-
-		if (soldierIdx != -1 && truckLoad < c && !soldiersMap.isSoldierRescued(soldierIdx)) {
-			newTruckLoad++;
-			newSoldiersMap.pickupSoldier(soldierIdx);
-		}
-
-		return new State(Location.getNewLocation(location, "PICK"), newTruckLoad, newSoldiersMap);
-	}
-
-	public State transition(State state, String operator) {
-		Location location = state.getLocation();
-		int truckLoad = state.getTruckLoad();
-		SoldiersMap soldiers = state.getSoldiers();
-
-		Location newLocation;
-
-		switch (operator) {
-		case "UP":
-			newLocation = Location.getNewLocation(location, "UP");
-			if (!Location.locationInBounds(newLocation, n, m))
-				newLocation = Location.getNewLocation(location, "DOWN");
-			return new State(newLocation, truckLoad, soldiers);
-		case "DOWN":
-			newLocation = Location.getNewLocation(location, "DOWN");
-			if (!Location.locationInBounds(newLocation, n, m))
-				newLocation = Location.getNewLocation(location, "UP");
-			return new State(newLocation, truckLoad, soldiers);
-		case "LEFT":
-			newLocation = Location.getNewLocation(location, "LEFT");
-			if (!Location.locationInBounds(newLocation, n, m))
-				newLocation = Location.getNewLocation(location, "RIGHT");
-			return new State(newLocation, truckLoad, soldiers);
-		case "RIGHT":
-			newLocation = Location.getNewLocation(location, "RIGHT");
-			if (!Location.locationInBounds(newLocation, n, m))
-				newLocation = Location.getNewLocation(location, "LEFT");
-			return new State(newLocation, truckLoad, soldiers);
-		case "DROP":
-			return stateAfterDrop(state);
-		default:
-			return stateAfterPick(state);
-		}
-	}
-
-	public int pathCost(Node node) {
-		if (node.getOperator().toString() != "PICK")
-			return 0;
-
-		int soldierIdx = getSoldierIndexAtLocation(node.getState().getLocation());
-
-		int cost = node.getPathCost();
-		int soldierHealth = 100 - soldiers[soldierIdx].getInitalDamage();
-		if (soldierHealth <= node.getDepth() * 2)
-			cost += soldierHealth + 10000;
-		else
-			cost += node.getDepth() * 2;
-
-		return cost;
-	}
-
-	public int getSoldierIndexAtLocation(Location location) {
-		int soldierIdx = -1;
-		for (int i = 0; i < soldiers.length && soldierIdx == -1; i++) {
-			Soldier soldier = soldiers[i];
-			if (soldier.getLocation().equals(location))
-				soldierIdx = i;
-		}
-		return soldierIdx;
-	}
-
-	@Override
-	public String toString() {
-		return "Problem [n=" + n + ", m=" + m + ", c=" + c + ", initialState=" + initialState + ", operators="
-				+ Arrays.toString(operators) + ", ethanLocation=" + ethanLocation + ", submarineLocation="
-				+ submarineLocation + ", soldiers=" + Arrays.toString(soldiers) + "]";
-	}
-
-	public String[] getOperators() {
-		return operators;
-	}
-
-	public void setOperators(String[] operators) {
-		this.operators = operators;
-	}
-
-	public int getN() {
-		return n;
-	}
-
-	public void setN(int n) {
-		this.n = n;
-	}
-
-	public int getM() {
-		return m;
-	}
-
-	public void setM(int m) {
-		this.m = m;
-	}
-
-	public int getC() {
-		return c;
-	}
-
-	public void setC(int c) {
-		this.c = c;
-	}
-
+	
+	public abstract boolean goalTest(State state);
+	
+	public abstract int pathCost(Node node);
+	
+	//state space represented as transition function
+	public abstract State transition(State state, String operator);
+	
 	public State getInitialState() {
 		return initialState;
 	}
 
-	public void setInitialState(State initialState) {
-		this.initialState = initialState;
-	}
-
-	public Location getEthanLocation() {
-		return ethanLocation;
-	}
-
-	public void setEthanLocation(Location ethanLocation) {
-		this.ethanLocation = ethanLocation;
-	}
-
-	public Location getSubmarineLocation() {
-		return submarineLocation;
-	}
-
-	public void setSubmarineLocation(Location submarineLocation) {
-		this.submarineLocation = submarineLocation;
-	}
-
-	public Soldier[] getSoldiers() {
-		return soldiers;
-	}
-
-	public void setSoldiers(Soldier[] soldiers) {
-		this.soldiers = soldiers;
+	public String[] getOperators() {
+		return operators;
 	}
 
 }
---
+++ src/mission/MapGenerator.java
@@ -0,0 +1,119 @@
+package mission;
+
+import java.util.HashSet;
+import core.State;
+import data.Location;
+import data.Soldier;
+import data.SoldiersMap;
+
+public class MapGenerator {
+	private static StringBuilder grid;
+	private static final int minGridRange = 5;
+	private static final int maxGridRange = 15;
+	private static final int minSoldierCount = 1;
+	private static final int maxSoldierCount = 10;
+	private static final int minHealth = 1;
+	private static final int maxHealth = 99;
+
+	private static int generateNumberWithinRange(int min, int max) {
+		return (int) (Math.random() * (max - min + 1)) + min;
+	}
+
+	private static int generateNumber(int max) {
+		return (int) (Math.random() * max);
+	}
+
+	public static String generate() {
+		StringBuilder health = new StringBuilder();
+		grid = new StringBuilder();
+		int m = generateNumberWithinRange(minGridRange, maxGridRange);
+		int n = generateNumberWithinRange(minGridRange, maxGridRange);
+		HashSet<Integer> fullCells = new HashSet<Integer>();
+		Location ethan = new Location(generateNumber(n), generateNumber(m));
+		fullCells.add(ethan.getX() * m + ethan.getY());
+		Location submarine;
+		while (true) {
+			int x = generateNumber(n);
+			int y = generateNumber(m);
+			int unique = x * m + y;
+			if (!fullCells.contains(unique)) {
+				submarine = new Location(x, y);
+				fullCells.add(unique);
+				break;
+			}
+		}
+
+		int soldiers = generateNumberWithinRange(minSoldierCount, maxSoldierCount);
+		int c = generateNumberWithinRange(minSoldierCount, maxSoldierCount);
+		append(m, n);
+		append(ethan.getX(), ethan.getY());
+		append(submarine.getX(), submarine.getY());
+		Soldier s;
+		for (int i = 0; i < soldiers; i++) {
+			while (true) {
+				int x = generateNumber(n);
+				int y = generateNumber(m);
+				int unique = x * m + y;
+				if (!fullCells.contains(unique)) {
+					s = new Soldier(new Location(x, y), generateNumberWithinRange(minHealth, maxHealth));
+					fullCells.add(unique);
+					break;
+				}
+			}
+
+			fullCells.add(s.getLocation().getX() * m + s.getLocation().getY());
+			appendSoldier(s.getLocation().getX(), s.getLocation().getY(), i + 1 < soldiers);
+			health.append(s.getInitalDamage());
+			if (i + 1 < soldiers)
+				health.append(",");
+		}
+		grid.append(";").append(health.toString()).append(";").append(c);
+		return grid.toString();
+	}
+
+	private static void append(int x, int y) {
+		grid.append(x).append(",").append(y).append(";");
+	}
+
+	private static void appendSoldier(int x, int y, Boolean comma) {
+		grid.append(x).append(",").append(y);
+		if (comma)
+			grid.append(",");
+	}
+
+    public static MissionImpossible parse(String grid) {
+		String[] splitter = grid.split(";");
+
+		String[] gridSize = splitter[0].split(",");
+
+		int m = Integer.parseInt(gridSize[0]);
+		int n = Integer.parseInt(gridSize[1]);
+
+		String[] ethan = splitter[1].split(",");
+		Location ethanLocation = new Location(Integer.parseInt(ethan[0]), Integer.parseInt(ethan[1]));
+
+		String[] submarine = splitter[2].split(",");
+		Location submarineLocation = new Location(Integer.parseInt(submarine[0]), Integer.parseInt(submarine[1]));
+
+		String[] soldierLocations = splitter[3].split(",");
+		String[] soldierHealths = splitter[4].split(",");
+		int numOfSoldiers = soldierLocations.length / 2;
+		Soldier[] soldiers = new Soldier[numOfSoldiers];
+
+		for (int i = 0; i < 2 * numOfSoldiers; i += 2) {
+			Soldier soldier = new Soldier(
+					new Location(Integer.parseInt(soldierLocations[i]), Integer.parseInt(soldierLocations[i + 1])),
+					Integer.parseInt(soldierHealths[i / 2]));
+			soldiers[i / 2] = soldier;
+		}
+
+		int truckCapacity = Integer.parseInt(splitter[5]);
+
+		String[] operators = { "UP", "DOWN", "LEFT", "RIGHT", "DROP", "PICK" };
+		
+		State intialState = new State(ethanLocation, 0, new SoldiersMap(numOfSoldiers));
+		
+		return new MissionImpossible(operators, intialState, n, m, truckCapacity, ethanLocation, submarineLocation, soldiers);
+	}
+
+}
