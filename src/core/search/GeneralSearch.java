5ab328781390aa683c31a4a2da962b32e1e1591c
Ronic <abdoronic@gmail.com>
2020-11-18 11:10:56 +0200

    added all tech. except for IDF

--- src/core/search/GeneralSearch.java
+++ src/core/search/GeneralSearch.java
@@ -8,10 +8,28 @@ import core.State;
 import core.Strategy;
 
 public class GeneralSearch {
-	
+
 	public static int totalExpanded;
 
-	public static ArrayList<Node> expand(Node currNode, Problem problem) {
+	public static int calculatePathCost(Node a, Node b, Problem problem, Strategy strategy) {
+		int g = a.getPathCost() + problem.pathCost(b);
+		int h1 = problem.h1(b);
+		int h2 = problem.h2(b);
+		switch (strategy) {
+		case GR1:
+			return h1;
+		case GR2:
+			return h2;
+		case AS1:
+			return g + h1;
+		case AS2:
+			return g + h2;
+		default:
+			return g;
+		}
+	}
+
+	public static ArrayList<Node> expand(Node currNode, Problem problem, Strategy strategy) {
 		State currentState = currNode.getState();
 		ArrayList<Node> expandedNodes = new ArrayList<>();
 		for (String operator : problem.getOperators()) {
@@ -19,7 +37,8 @@ public class GeneralSearch {
 			if (nextState.equals(currentState))
 				continue;
 			Node expandedNode = new Node(currNode, nextState, operator, currNode.getDepth() + 1, 0);
-			expandedNode.setPathCost(currNode.getPathCost() + problem.pathCost(expandedNode));
+			int pathCost = calculatePathCost(currNode, expandedNode, problem, strategy);
+			expandedNode.setPathCost(pathCost);
 			expandedNodes.add(expandedNode);
 		}
 		return expandedNodes;
@@ -33,7 +52,7 @@ public class GeneralSearch {
 			Node current = tree.pop();
 			if (problem.goalTest(current.getState()))
 				return current;
-			ArrayList<Node> children = expand(current, problem);
+			ArrayList<Node> children = expand(current, problem, strategy);
 			totalExpanded++;
 			for (Node node : children)
 				if (!tree.isVisited(node))
